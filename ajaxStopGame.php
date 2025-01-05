ddcc4ebbe90f806cc0805759945173c4288c0774
isk727 <isk727@gmail.com>
2025-01-06 00:53:37 +0900

    Update ajaxStopGame.php

--- ajaxStopGame.php
+++ ajaxStopGame.php
@@ -2,9 +2,12 @@
 if (is_file('../class/Game.class.php')) { require_once('../class/Game.class.php'); }
 $gb = new GameBean();
 $req = json_decode(file_get_contents("php://input"), true);
-//data = { "id" : id, "ip" : ip, "gkey" : gkey,};
-$ret = $gb->stopGame($req['id'], $req['ip'], $req['gkey']);
-//$ret = "河野太郎=" . $req['gkey'];
+$ret = "";
+if ($req['ft'] == 1) {
+  $ret = $gb->forceTermGame($req['id'], $req['gkey']);
+} else {
+  $ret = $gb->stopGame($req['id'], $req['ip'], $req['gkey']);
+}
 $result =[ "value" => $ret, ];
 $json = json_encode($result, JSON_UNESCAPED_UNICODE);
 header("Content-Type: application/json; charset=UTF-8");
